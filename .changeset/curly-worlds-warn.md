c127072b1443aeb2137196b7237bd479460d814d
haili.lhq <haili.lhq@alibaba-inc.com>
2025-12-03 11:45:30 +0800

    refactor: migrate to new repo

---
+++ .changeset/curly-worlds-warn.md
@@ -0,0 +1,6 @@
+---
+'create-qbi-app': major
+'@quickbi/qbi-dev-tools': patch
+---
+
+Refactor and migrate to new a repo
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,20 @@
 import js from '@eslint/js';
 import globals from 'globals';
 import ts from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-export default [
+export default defineConfig([
   { languageOptions: { globals: globals.node } },
   js.configs.recommended,
   ...ts.configs.recommended,
   {
-    ignores: ['**/dist/**', 'packages/create-qbi-app/template-*'],
+    ignores: [
+      '**/dist/**',
+      'packages/create-qbi-app/template-*',
+      '**/eslint.config.mjs',
+      '**/rslib.config.ts',
+      '**/commitlint.config.mjs',
+    ],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
     },
@@ -18,4 +25,4 @@ export default [
       },
     },
   },
-];
+]);
